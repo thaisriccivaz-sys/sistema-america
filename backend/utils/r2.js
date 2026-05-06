17001592386af8a37e7e8591bf96e82c530be062
Thais Ricci <SEUEMAILDOGITHUB>
2026-05-06 12:48:40 -0300

    fix: remove s3-request-presigner

--- backend/utils/r2.js
+++ backend/utils/r2.js
@@ -1,5 +1,4 @@
 const { S3Client, PutObjectCommand, GetObjectCommand } = require("@aws-sdk/client-s3");
-const { getSignedUrl } = require("@aws-sdk/s3-request-presigner");
 const fs = require('fs');
 
 const R2_ACCESS_KEY_ID = process.env.R2_ACCESS_KEY_ID;
